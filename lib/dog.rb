7764b667ae714ae941c6aaf2ec49178db4139d65
Shun Yao <ysmiracle@hotmail.com>
2018-04-12 02:55:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,10 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(id=nil, attributes)
+  def initialize(id=nil, name:, breed:)
     @id = id
-    attributes.each {|key, value| self.send("#{key}=", value)}
+    @name = name
+    @breed = breed
   end
 
   def self.create_table
